83ff51372227702178f11d0a9d4a7b80d499fb0c
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2023-12-19 23:32:48 +0530

    Remove changes in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    //remove changes
     <html lang="en">
       <body className={inter.className}>
         <RegisterModal />
