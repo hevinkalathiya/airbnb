8af6533ff58d00e05f7785ccd68059648f2a8b64
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2023-12-28 23:55:27 +0530

    Refactor getCurrentUser function

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,34 +1,32 @@
-//! Temporarily disabled beacuse its breaking the build
+import { getServerSession } from "next-auth/next";
 
-// import { getServerSession } from "next-auth/next";
+import prisma from "@/app/lib/prismadb";
+import { authOptions } from "../api/auth/[...nextauth]/option";
 
-// import prisma from "@/app/lib/prismadb";
-// import { authOptions } from "../api/auth/[...nextauth]/option";
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
 
-// export async function getSession() {
-//   return await getServerSession(authOptions);
-// }
+export async function getCurrentUser() {
+  try {
+    const session = await getSession();
 
-// export async function getCurrentUser() {
-//   try {
-//     const session = await getSession();
+    if (!session?.user?.email) {
+      return null;
+    }
 
-//     if (!session?.user?.email) {
-//       return null;
-//     }
-
-//     const currentUser = await prisma.user.findUnique({
-//       where: {
-//         email: session?.user?.email as string,
-//       },
-//     });
-//     if (!currentUser) {
-//       console.error("User not found");
-//       return null;
-//     }
-//     return currentUser;
-//   } catch (error) {
-//     console.error(error, "Error getting current user");
-//     return null;
-//   }
-// }
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session?.user?.email as string,
+      },
+    });
+    if (!currentUser) {
+      console.error("User not found");
+      return null;
+    }
+    return currentUser;
+  } catch (error) {
+    console.error(error, "Error getting current user");
+    return null;
+  }
+}
