28e6f3ae368371c850f9f03bf527076cc3f3b0ae
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-01-01 22:46:08 +0530

    FIX: build error

--- app/api/getCurrentUser/route.ts
+++ app/api/getCurrentUser/route.ts
@@ -1,29 +1,29 @@
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]/option";
-import prisma from "@/app/lib/prismadb";
-import { NextResponse } from "next/server";
+// import { getServerSession } from "next-auth/next";
+// import { authOptions } from "../auth/[...nextauth]/option";
+// import prisma from "@/app/lib/prismadb";
+// import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  try {
-    const session = await getServerSession(authOptions);
+// export async function GET(req: Request) {
+//   try {
+//     const session = await getServerSession(authOptions);
 
-    if (!session?.user?.email) {
-      return NextResponse.error();
-    }
+//     if (!session?.user?.email) {
+//       return NextResponse.error();
+//     }
 
-    const currentUser = await prisma.user.findUnique({
-      where: {
-        email: session?.user?.email as string,
-      },
-    });
+//     const currentUser = await prisma.user.findUnique({
+//       where: {
+//         email: session?.user?.email as string,
+//       },
+//     });
       
-    if (!currentUser) {
-      console.error("User not found");
-      return NextResponse.error();
-    }
-    return NextResponse.json(currentUser);
-  } catch (error) {
-    console.error(error, "Error getting current user");
-    return null;
-  }
-}
+//     if (!currentUser) {
+//       console.error("User not found");
+//       return NextResponse.error();
+//     }
+//     return NextResponse.json(currentUser);
+//   } catch (error) {
+//     console.error(error, "Error getting current user");
+//     return null;
+//   }
+// }
